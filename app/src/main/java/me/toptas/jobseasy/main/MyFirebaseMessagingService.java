572c17c53e91a2854a3de1e5613927f77b734118
sreenathm99 <sreenathmenon97@gmail.com>
2017-10-23 01:31:48 +0530

    push .. sundy 23.10.17 image notification

--- app/src/main/java/me/toptas/jobseasy/main/MyFirebaseMessagingService.java
+++ app/src/main/java/me/toptas/jobseasy/main/MyFirebaseMessagingService.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -14,10 +16,15 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import me.toptas.jobseasy.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
+    Bitmap bitmap;
 
     /**
      * Called when message is received.
@@ -33,12 +40,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-            sendNotification("text");
+
+
+
         }
 // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+
+
+
+
         }
+        //To get a Bitmap image from the URL received
+
+        String imageUri = remoteMessage.getData().get("image");
+        bitmap = getBitmapfromUrl(imageUri);
+
+
+        sendNotification("text",bitmap);
+
+
     }
 
 
@@ -47,21 +69,43 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param messageBody FCM message body received.
      */
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String messageBody, Bitmap image) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                 .setLargeIcon(image)
                 .setSmallIcon(R.drawable.new_noti)
                 .setContentTitle("FCM Message")
                 .setContentText(messageBody)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))/*Notification with Image*/
                 .setAutoCancel(true)
+
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
+    }
+
 }
